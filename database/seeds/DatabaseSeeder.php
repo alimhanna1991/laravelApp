8bdc88ec52b15bc93c04b83edac174f846abc4e0
alimhanna1991 <33596355+alimhanna1991@users.noreply.github.com>
2022-01-16 11:20:17 +0200

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        Post::factory(5)->create();
+
+
+    }
+}
