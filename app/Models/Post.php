9cabc240f48ef2bae3f7363e9ddcce2dff63d65a
alimhanna1991 <33596355+alimhanna1991@users.noreply.github.com>
2022-01-17 13:42:13 +0200

    Ep 39
    Episode 39 code

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Post extends Model
 {
@@ -18,10 +19,23 @@ class Post extends Model
     public function author(){
         return $this->belongsTo(User::class , 'user_id');
         }
-    public function scopeFilter($query){
-        if(request('search')){
-            $query->where('title','like','%'.request('search').'%')
-            ->orWhere('body','like','%'.request('search').'%');
-    }
+    public function scopeFilter($query ,array $filters){
+        $query->when($filters['search']?? false ,fn($query,$search)=>
+        $query->where('title','like','%'.$search.'%')
+            ->orWhere('body','like','%'.$search .'%')
+    );
+    $query->when($filters['category']?? false ,fn($query,$category)=>
+    $query->whereHas('category',fn($query)=>$query->where('slug',$category)));
+    /*
+    First Way
+    */
+//     $query->when($filters['category']?? false ,fn($query,$category)=>
+//     $query->whereExists(fn($query)=>
+//         $query->from('categories')->whereColumn('categories.id','posts.category_id')
+//                                 ->where('categories.slug',$category)
+
+//     )
+// );
+
     }
 }
