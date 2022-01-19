831312e4a797809bae69e2693fdf608071cb1999
alimhanna1991 <33596355+alimhanna1991@users.noreply.github.com>
2022-01-19 13:42:43 +0200

    ep 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,8 +15,12 @@
                     type="text"
                     name="name"
                     id='name'
+                    value="{{old('name')}}"
                     required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                  <div class="mb-6">
@@ -29,8 +33,12 @@
                      type="text"
                      name="username"
                      id='username'
+                     value="{{old('username')}}"
                      required
                      >
+                     @error('username')
+                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                 @enderror
                  </div>
 
                  <div class="mb-6">
@@ -43,8 +51,12 @@
                     type="email"
                     name="email"
                     id='email'
+                    value="{{old('email')}}"
                     required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
                 </div>
 
                  <div class="mb-6">
@@ -57,8 +69,12 @@
                     type="password"
                     name="password"
                     id='password'
+
                     required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
                 </div>
 
                 <div class="mb-6">
@@ -69,6 +85,15 @@
                     required
                     >Submit</button>
                 </div>
+                {{-- @if ($errors->any)
+                <ul>
+                    @foreach ($errors->all() as $error )
+                    <li>{{$error}}</li>
+                     @endforeach
+                </ul>
+                @endif --}}
+
+
             </form>
 
         </main>
