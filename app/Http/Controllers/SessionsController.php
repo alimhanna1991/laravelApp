cd84126815c504bc2b10c605491d453668a0f367
alimhanna1991 <33596355+alimhanna1991@users.noreply.github.com>
2022-01-20 11:05:25 +0200

    ep50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
 
 class SessionsController extends Controller
@@ -10,4 +11,32 @@ class SessionsController extends Controller
         auth()->logout();
         return redirect('/')->with('success','GoodBye!');
     }
+    public function create(){
+       return view('sessions.create');
+    }
+    public function store(){
+        // Validate Input
+        $att = request()->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        if(auth()->attempt($att)){
+            session()->regenerate();
+            // redirect with success message
+            return redirect('/')->with('success','Welcome Back');
+        }
+        // auth fail
+        // First way
+        return back()
+                 ->withInput()
+                 ->withErrors(['email'=>'Your provided credentials could not be verified']);
+
+        //Second Way
+
+    //    return throw ValidationException::withMessages([
+    //         'email'=>'Your provided credentials could not be verified'
+    //     ]);
+
+    }
+
 }
